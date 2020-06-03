828c800cce1ca608039a9cdd2194b6db770f830d
hterral888 <59943188+hterral888@users.noreply.github.com>
2020-06-02 19:03:53 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,2 +1,22 @@
-def hello():
-  print('hello')
+def process_pdf_files(list_of_file_names):
+  resource_manager = PDFResourceManager()
+  fake_file_handle = io.StringIO()
+  converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+  page_interpreter = PDFPageInterpreter(resource_manager, converter)
+  
+  text_dictionary = {}
+  for name in list_of_file_names:
+    with open(name, 'rb') as fh:
+
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+
+        text = fake_file_handle.getvalue()
+        text_dictionary[name] = text
+
+  # close open handles
+  converter.close()
+  fake_file_handle.close()
+  return text_dictionary
