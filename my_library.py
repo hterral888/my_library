e9668e4e2d057e1511550179ba1d780d507e6037
hterral888 <59943188+hterral888@users.noreply.github.com>
2020-06-06 17:40:54 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,17 +1,41 @@
+def installs:
+  !pip install pdfminer3
+  !pip install spacy 
+  
+  
+def imports:
+  from pdfminer3.layout import LAParams, LTTextBox
+  from pdfminer3.pdfpage import PDFPage
+  from pdfminer3.pdfinterp import PDFResourceManager
+  from pdfminer3.pdfinterp import PDFPageInterpreter
+  from pdfminer3.converter import PDFPageAggregator
+  from pdfminer3.converter import TextConverter
+  import io
+  import spacy 
+  !python -m spacy download en_core_web_md
+  import en_core_web_md
+  nlp = en_core_web_md.load()
+
+
 def process_pdf_files(list_of_file_names):
   resource_manager = PDFResourceManager()
   fake_file_handle = io.StringIO()
   converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
   page_interpreter = PDFPageInterpreter(resource_manager, converter)
-  
   text_dictionary = {}
   for name in list_of_file_names:
-    with open(name, 'rb') as fh:
-
+    print(name)
+    with open(entries / name, 'rb') as fh:
+        i = 0
         for page in PDFPage.get_pages(fh,
                                       caching=True,
                                       check_extractable=True):
-            page_interpreter.process_page(page)
+            print(i)
+            i += 1
+            try:
+              page_interpreter.process_page(page)
+            except:
+              print(i)
 
         text = fake_file_handle.getvalue()
         text_dictionary[name] = text
@@ -20,3 +44,5 @@ def process_pdf_files(list_of_file_names):
   converter.close()
   fake_file_handle.close()
   return text_dictionary
+
+
