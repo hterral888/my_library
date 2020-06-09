a59185e8f030651b43e03ca5b2a226f355a59da5
hterral888 <59943188+hterral888@users.noreply.github.com>
2020-06-09 12:29:35 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -26,7 +26,7 @@ def get_list():
         district_list.append(entry.name)
     print(len(district_list), district_list)
     alpha_district_list = sorted(district_list)
-    return print(alpha_district_list)
+    return alpha_district_list
 
 def process_pdf_files(list_of_file_names):
   resource_manager = PDFResourceManager()
